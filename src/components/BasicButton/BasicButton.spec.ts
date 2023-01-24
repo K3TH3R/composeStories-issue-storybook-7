8eb436e3d9489bd3d1e1d3878d556f4544e74a81
Kether Saturnius <kether@trendspek.com>
2023-01-24 15:19:39 +1100

    react-types-issue ensure test fails on first run

--- src/components/BasicButton/BasicButton.spec.ts
+++ src/components/BasicButton/BasicButton.spec.ts
@@ -1,6 +1,5 @@
 import * as stories from './BasicButton.stories.js'
 import { composeStories } from '@storybook/testing-vue3'
-// @ts-expect-error - there's an issue with the types of composeStories
 const composed = composeStories(stories)
 
 describe(`BasicButton.vue`, () => {
